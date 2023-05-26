d5bbb42b3eddcd98f1c04859d5da084df61d4c1e
Lucas Wolff <lucashwolff@gmail.com>
2023-05-26 16:02:56 -0300

    3-features layer

--- src/components/UserCards.jsx
+++ src/components/UserCards.jsx
@@ -1,63 +1,10 @@
-import { useEffect, useState } from "react";
-import JohnDoe from "../assets/images/john-doe.jpeg";
-import JaneDoe from "../assets/images/jane-doe.jpeg";
-import PixelDoe from "../assets/images/pixel-doe.jpeg";
 import { getAgeFromBirthdate } from "../utils/date";
 import { openCallPhoneNumber, openSendEmail } from "../utils/contact";
+import { useFetchUsers } from "../hooks/useFetchUsers";
 import "../index.css";
 
-const fetchUsers = () =>
-  new Promise((resolve) =>
-    setTimeout(() => {
-      resolve([
-        {
-          id: "1",
-          name: "John Doe",
-          birthdate: "1988-01-01",
-          role: "Full Stack Developer",
-          profilePictureUrl: JohnDoe,
-          email: "user1@mail.com",
-          phoneNumber: "555555555",
-        },
-        {
-          id: "2",
-          name: "Jane Doe",
-          birthdate: "1994-01-01",
-          role: "Front-end Developer",
-          profilePictureUrl: JaneDoe,
-          email: "user2@mail.com",
-          phoneNumber: "555555555",
-        },
-        {
-          id: "3",
-          name: "Pixel Doe",
-          birthdate: "1960-01-01",
-          role: "Back-end Developer",
-          profilePictureUrl: PixelDoe,
-          email: "user3@mail.com",
-          phoneNumber: "555555555",
-        },
-      ]);
-    }, 1000)
-  );
-
 const UserCards = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetchUsers()
-      .then((data) => {
-        setUsers(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
+  const { users, isLoading } = useFetchUsers();
 
   const handleCallClick = (phoneNumber) => {
     openCallPhoneNumber(phoneNumber);
